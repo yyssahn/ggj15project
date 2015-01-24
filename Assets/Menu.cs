693f9ba24f63265fee29c42e5bce93275cec272c
Jade He <jadehe@live.com>
2015-01-24 15:00:38 -0800

    gameover

---
+++ Assets/Menu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+	void OnGUI ()
+	{
+		if (GUI.Button (new Rect (Screen.width / 2.5f, Screen.height / 3, Screen.width / 5, Screen.height / 10), "Load Game")) 
+		{
+			Application.LoadLevel(1);
+		}
+
+
+		if (GUI.Button (new Rect (Screen.width / 2.5f, Screen.height / 3, Screen.width / 5, Screen.height / 10), "Load Game")) 
+		{
+			Application.Quit();
+		}
+
+
+	}
+}
