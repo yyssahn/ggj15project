a6edcac2d6598cfb7682e9c8a39a70dbb5be8ece
ys <yyssahn@gmail.com>
2015-01-23 22:53:58 -0800

    shit stuff

---
+++ Assets/movement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+
+	void Update ()
+	{
+
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			transform.Translate (Vector3.right * 5 * Time.deltaTime);
+			print ("help");
+		}
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			transform.Translate (Vector3.left * 5 * Time.deltaTime);
+		}
+		
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			transform.Translate (Vector3.up * 5 * Time.deltaTime);
+			
+		}
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			transform.Translate (Vector3.down * 5 * Time.deltaTime);
+		}
+		
+	}
+
+}
